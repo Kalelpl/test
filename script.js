400fd313487140451a6ccb313b526122beb68c3c
Kalelpl <36899631+Kalelpl@users.noreply.github.com>
2018-04-16 17:36:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+
 function openNav() {
      document.getElementById("mySidenav").style.width = "25%"
  }
 
+
  function closeNav() {
      document.getElementById("mySidenav").style.width = "0";
  }
+
+function myFunction(x) {
+    if (x.matches) 
+        function openNav() {
+     document.getElementById("mySidenav").style.width = "100%"
+ }
+ function closeNav() {
+     document.getElementById("mySidenav").style.width = "0";
+ }
+}
+
+var x = window.matchMedia("(max-width: 440px)")
+x.addListener(myFunction)
+
